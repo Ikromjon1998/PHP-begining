8c9595010b2e319c481ece43eedf63ae4d15a7b5
Ikromjon Ochilov <s288560@studenti.polito.it>
2022-03-28 16:57:45 +0200

    basic concepts

---
+++ Chapter 1/2-1.php
@@ -0,0 +1,16 @@
+<?php
+    $name = "Ikromjon"; //variable
+?>
+
+<html>
+    <head>
+        <title>Variable</title>
+    </head>
+    <body>
+        <p>
+            <strong>
+                Welcome to PHP, <?php print("$name"); ?>!
+            </strong>
+        </p>
+    </body>
+</html>
---
+++ Chapter 1/3-1.php
@@ -0,0 +1,15 @@
+
+<html>
+    <head></head>
+    <body>
+        <?php
+            $d = date("D");
+            if($d == "Wed")
+                echo "it is Wednesday";
+            if($d == "Mon")
+                echo "it is Monday";
+            else 
+                echo "Have a nice day!";
+        ?>
+    </body>
+</html>
